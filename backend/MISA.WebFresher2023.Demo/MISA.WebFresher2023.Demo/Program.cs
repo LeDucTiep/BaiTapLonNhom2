7909659381992b0f4fc60049c32cbd54a2c8f32e
LeDucTiep <92147779+LeDucTiep@users.noreply.github.com>
2023-07-27 18:34:55 +0700

    27/07/2023

---
+++ backend/MISA.WebFresher2023.Demo/MISA.WebFresher2023.Demo/Program.cs
@@ -0,0 +1,141 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MISA.WebFresher2023.Demo.BL.Service;
+using MISA.WebFresher2023.Demo.DL;
+using MISA.WebFresher2023.Demo.DL.Repository;
+using MISA.WebFresher2023.Demo.Middleware;
+using MISA.WebFresher2023.Demo.Common;
+using FirebaseAdmin;
+using DocumentFormat.OpenXml.Office2016.Drawing.ChartDrawing;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authentication;
+
+WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+
+// Lấy data từ file 
+builder.ReadConfig();
+
+// Tạo config instance
+
+AppConfig appConfig = builder.Configuration.GetConfig();
+
+builder.Services.AddSingleton(appConfig);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+
+// Định dạnh các thuộc tính trả về là kiểu Pascal
+builder.Services.AddControllers().AddJsonOptions(option =>
+{
+    option.JsonSerializerOptions.PropertyNamingPolicy = null;
+
+});
+
+// Firebase 
+builder.Services.AddSingleton(FirebaseApp.Create());
+
+builder.Services
+                .AddAuthentication(options =>
+                {
+                    options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+                })
+                .AddScheme<AuthenticationSchemeOptions, FirebaseAuthenticationHandler>(JwtBearerDefaults.AuthenticationScheme, (o) => { });
+
+builder.Services.AddScoped<FirebaseAuthenticationFunctionHandler>();
+
+//builder.Services.AddFirebaseAuthentication();
+
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// Thêm Cors cho font-end
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: "MyVueApp",
+                      policy =>
+                      {
+                          policy.WithOrigins("*")
+                            .AllowAnyHeader()
+                            .AllowAnyMethod();
+                      });
+});
+
+//builder.Services.Configure<ApiBehaviorOptions>(options =>
+//{
+//    options.SuppressModelStateInvalidFilter = true;
+//});
+
+// Sử dụng automapper
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+builder.Services.AddScoped<IEmployeeRepository, EmployeeRepository>();
+builder.Services.AddScoped<IPositionRepository, PositionRepository>();
+builder.Services.AddScoped<IDepartmentRepository, DepartmentRepository>();
+builder.Services.AddScoped<IAccountRepository, AccountRepository>();
+builder.Services.AddScoped<IAccountPropertyRepository, AccountPropertyRepository>();
+builder.Services.AddScoped<ICustomerRepository, CustomerRepository>();
+builder.Services.AddScoped<ICustomerAndGroupRepository, CustomerAndGroupRepository>();
+builder.Services.AddScoped<ICustomerGroupRepository, CustomerGroupRepository>();
+builder.Services.AddScoped<IContactInforRepository, ContactInforRepository>();
+builder.Services.AddScoped<ITermsOfPaymentRepository, TermsOfPaymentRepository>();
+builder.Services.AddScoped<IBankAccountRepository, BankAccountRepository>();
+builder.Services.AddScoped<INationRepository, NationRepository>();
+builder.Services.AddScoped<ICityRepository, CityRepository>();
+builder.Services.AddScoped<IDistrictRepository, DistrictRepository>();
+builder.Services.AddScoped<ICommuneRepository, CommuneRepository>();
+builder.Services.AddScoped<IOtherLocationRepository, OtherLocationRepository>();
+builder.Services.AddScoped<ISpecificAddressRepository, SpecificAddressRepository>();
+builder.Services.AddScoped<IReceiptRepository, ReceiptRepository>();
+builder.Services.AddScoped<IBookKeepingRepository, BookKeepingRepository>();
+
+builder.Services.AddScoped<IEmployeeService, EmployeeService>();
+builder.Services.AddScoped<IPositionService, PositionService>();
+builder.Services.AddScoped<IDepartmentService, DepartmentService>();
+builder.Services.AddScoped<IAccountService, AccountService>();
+builder.Services.AddScoped<IAccountPropertyService, AccountPropertyService>();
+builder.Services.AddScoped<ICustomerService, CustomerService>();
+builder.Services.AddScoped<ICustomerAndGroupService, CustomerAndGroupService>();
+builder.Services.AddScoped<ICustomerGroupService, CustomerGroupService>();
+builder.Services.AddScoped<IContactInforService, ContactInforService>();
+builder.Services.AddScoped<ITermsOfPaymentService, TermsOfPaymentService>();
+builder.Services.AddScoped<IBankAccountService, BankAccountService>();
+builder.Services.AddScoped<INationService, NationService>();
+builder.Services.AddScoped<ICityService, CityService>();
+builder.Services.AddScoped<IDistrictService, DistrictService>();
+builder.Services.AddScoped<ICommuneService, CommuneService>();
+builder.Services.AddScoped<IOtherLocationService, OtherLocationService>();
+builder.Services.AddScoped<ISpecificAddressService, SpecificAddressService>();
+builder.Services.AddScoped<IReceiptService, ReceiptService>();
+builder.Services.AddScoped<IBookKeepingService, BookKeepingService>();
+
+builder.Services.AddScoped<IMSDatabase, MSDatabase>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+
+
+app.UseCors("MyVueApp");
+
+app.UseHttpsRedirection();
+
+app.UseRouting();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.UseEndpoints(endpoints => endpoints.MapControllers());
+
+app.MapControllers();
+
+app.UseMiddleware<ExceptionMiddleware>();
+
+app.Run();
